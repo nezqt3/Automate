0ddcb462b9751c027431d180fdb57fef80b2393e
Denis <dalekseenko8@gmail.com>
2025-12-02 11:38:55 +0300

    add footer to project

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Advantages from "./components/Advantages";
 import Difference from "./components/Difference";
 import Costs from "./components/Costs";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Difference />
 
       <Costs />
+      <Footer />
     </div>
   );
 }
